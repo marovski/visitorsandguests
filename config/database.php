46c3896ca4b001363e3c180924617fcee7bcab47
Marovski <cardozo27cv@gmail.com>
2018-08-27 01:10:55 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '89.152.31.136'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'nan'),
+            'username' => env('DB_USERNAME', 'jose'),
+            'password' => env('DB_PASSWORD', 'jose.pt.&'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_general_ci',
